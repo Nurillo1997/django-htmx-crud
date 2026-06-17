9e01695355a768c882c6b629fc5779ce04394f0e
Nurillo1997 <nurilloabdurazzoqov@gmail.com>
2026-06-17 14:22:27 +0500

    fix: correct STATIC_URL prefix to /static/
    STATIC_URL was set to 'uiapp/static/' which doesn't match the path
    used in templates ({% static 'scripts/index-page.js' %}). This worked
    locally under runserver's dev static serving but produced 404s in
    production after collectstatic, breaking the JET table and edit form
    JavaScript entirely.

--- base/settings.py
+++ base/settings.py
@@ -71,7 +71,7 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'uiapp/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
